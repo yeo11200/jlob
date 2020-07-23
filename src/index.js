e7238f080da66da995b5d71abec8b37ca6225c06
Shin Jinseop <42762091+yeo11200@users.noreply.github.com>
2020-07-23 09:06:17 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import jQuery from 'jquery';
+windows.$ = window.jQuery = jQuery;
 ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
