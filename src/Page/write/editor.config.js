324ea4ca5584d2bf4df7bdb2e5c39e32fd06501a
shinjinseop <yeo11200@hanmail.net>
2020-08-20 22:22:14 +0900

    no message

--- src/Page/write/editor.config.js
+++ src/Page/write/editor.config.js
@@ -2,16 +2,15 @@ export var formats = [
     'header', 'font', 'size',
     'bold', 'italic', 'underline', 'strike', 'blockquote',
     'list', 'bullet', 'indent',
-    'link', 'image', 'video'
+    'link', 'image', 'video', 'color'
   ]
 
   export var modules = {
     toolbar: [
-        [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
+        [{ 'header': [1, 2, 3, 4, 5, 6, false]}],
         [{size: []}],
-        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-        [{'list': 'ordered'}, {'list': 'bullet'}, 
-         {'indent': '-1'}, {'indent': '+1'}],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote', 'color'],
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'color': ['#000000', '#e60000', '#ff9900', '#ffff00', '#008a00', '#0066cc', '#9933ff', '#ffffff', '#facccc', '#ffebcc', '#ffffcc', '#cce8cc', '#cce0f5', '#ebd6ff', '#bbbbbb', '#f06666', '#ffc266', '#ffff66', '#66b966', '#66a3e0', '#c285ff', '#888888', '#a10000', '#b26b00', '#b2b200', '#006100', '#0047b2', '#6b24b2', '#444444', '#5c0000', '#663d00', '#666600', '#003700', '#002966', '#3d1466', 'custom-color'] }, { 'background': [] }, 'link', 'emoji'],
         ['link', 'image', 'video'],
         ['clean']
       ],
