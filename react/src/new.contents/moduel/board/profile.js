3fec4aa2ed134c31878df9101737884065a43a50
shinjinseop <yeo11200@hanmail.net>
2020-09-30 01:08:43 +0900

    no message

--- react/src/new.contents/moduel/board/profile.js
+++ react/src/new.contents/moduel/board/profile.js
@@ -1,10 +1,16 @@
 import React from "react";
+import axios from 'axios';
+import ReactLoading from 'react-loading';
+import { IconButton} from '@material-ui/core';
+import { FirstPage, LastPage, KeyboardArrowRight, KeyboardArrowLeft} from '@material-ui/icons'
 
 
 class Profile extends React.Component{
 
     render(){
         console.log('Profile');
+
+          
         return(
             <div>
                 <h2>이력서</h2>
--- react/src/new.contents/route/router.js
+++ react/src/new.contents/route/router.js
@@ -1,13 +1,1 @@
-
-
-
-import React from 'react';
-
-class test extends React.Component{
-
-    constructor(){
-        console.log(1);
-    }
-}
-
 export { default as Profile } from '../moduel/board/profile';
