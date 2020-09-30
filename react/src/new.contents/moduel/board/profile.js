8da40515d255e2a73a3d812da22e7df2002ee93d
shinjinseop <yeo11200@hanmail.net>
2020-09-30 23:12:03 +0900

    [Modify] material 이력서 table 추가

--- react/src/new.contents/moduel/board/profile.js
+++ react/src/new.contents/moduel/board/profile.js
@@ -1,19 +1,60 @@
 import React from "react";
 import axios from 'axios';
 import ReactLoading from 'react-loading';
-import { IconButton} from '@material-ui/core';
+import { TableContainer, Table, TableHead, TableRow, TableCell, Paper, TableBody } from '@material-ui/core';
 import { FirstPage, LastPage, KeyboardArrowRight, KeyboardArrowLeft} from '@material-ui/icons'
 
 
 class Profile extends React.Component{
 
+      createData = function(name, calories, fat, carbs, protein) {
+        return { name, calories, fat, carbs, protein };
+      }
+
+      goToView = function(items){
+        console.log(items);
+      }
     render(){
         console.log('Profile');
-
+          
+          const rows = [
+            this.createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+            this.createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+            this.createData('Eclair', 262, 16.0, 24, 6.0),
+            this.createData('Cupcake', 305, 3.7, 67, 4.3),
+            this.createData('Gingerbread', 356, 16.0, 49, 3.9),
+          ];
           
         return(
             <div>
                 <h2>이력서</h2>
+
+                <TableContainer component={Paper}>
+                    <Table aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Dessert (100g serving)</TableCell>
+                                <TableCell align="right">Calories</TableCell>
+                                <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                        {rows.map((row) => (
+                            <TableRow key={row.name} onClick={ () => this.goToView(row.name)}>
+                                <TableCell component="th" scope="row">
+                                    {row.name}
+                                </TableCell>
+                                <TableCell align="right">{row.calories}</TableCell>
+                                <TableCell align="right">{row.fat}</TableCell>
+                                <TableCell align="right">{row.carbs}</TableCell>
+                                <TableCell align="right">{row.protein}</TableCell>
+                            </TableRow>
+                        ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </div>
         )
     }
