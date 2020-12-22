541d74235cb796610ffda0d58a9c360bd1789970
shinjinseop <yeo11200@hanmail.net>
2020-12-22 17:52:57 +0900

    no message

--- src/common.js
+++ src/common.js
@@ -1,4 +1,6 @@
-export var API_SERVER = window.location.host.indexOf('localhost') >= 0 ? '//localhost:5000' : 'https://jinseop-todo-list.herokuapp.com';
+// export var API_SERVER = '//jinseop-todo-list.herokuapp.com';
+
+export var API_SERVER = '//13.124.87.240';
 
 // export var API_SERVER = 'https://jinseop-todo-list.herokuapp.com';
 
